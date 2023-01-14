c728d7a9a509a5407065113f39d46bf2badad511
Eliseu <eliseusantico@gmail.com>
2023-01-14 20:29:29 -0300

    init commit

---
+++ src/main/java/com/viacep/consumocep/ConsumoCepApplication.java
@@ -0,0 +1,13 @@
+package com.viacep.consumocep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumoCepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumoCepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viacep/consumocep/ConsumoCepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viacep.consumocep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumoCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
