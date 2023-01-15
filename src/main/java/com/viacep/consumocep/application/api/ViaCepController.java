29e5d66cabe0b59abc224b36a7d0e6eb40dbea0b
Eliseu <eliseusantico@gmail.com>
2023-01-15 18:12:02 -0300

    refatorando controller

--- src/main/java/com/viacep/consumocep/application/api/ViaCepController.java
+++ src/main/java/com/viacep/consumocep/application/api/ViaCepController.java
@@ -1,6 +1,5 @@
 package com.viacep.consumocep.application.api;
 
-import com.viacep.consumocep.application.service.ViaCEPClient;
 import com.viacep.consumocep.application.service.ViaCepService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -8,15 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Log4j2
 @RequiredArgsConstructor
-public class CepViaCepController implements ViaCepAPI {
+public class ViaCepController implements ViaCepAPI {
 
         private final ViaCepService viaCepService;
 
         @Override
         public EnderecoDetalhadoResponse buscaCep(String cep) {
-            log.info("[start] CepController - buscaCep ");
+            log.info("[start] ViaCepController - buscaCep ");
             EnderecoDetalhadoResponse cepDetalhado = viaCepService.buscaCep(cep);
-            log.info("[finish] CepController - buscaCep ");
+            log.info("[finish] ViaCepController - buscaCep ");
             return cepDetalhado;
         }
     }
