49c3b108d7dcb13bba2804761fe94c875dc5dbb6
Eliseu <eliseusantico@gmail.com>
2023-01-14 21:35:02 -0300

    adicionando estrutura do projeto, ativando FeingClients na aplicacao

--- src/main/java/com/viacep/consumocep/ConsumoCepApplication.java
+++ src/main/java/com/viacep/consumocep/ConsumoCepApplication.java
@@ -2,8 +2,12 @@ package com.viacep.consumocep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
+
 public class ConsumoCepApplication {
 
 	public static void main(String[] args) {
